7109e2b62a5c16a1c6667e6c7e2407155f553098
triple98s <saidbinsuleiman5@gmail.com>
2026-09-03 21:26:59 +0300

    Create configured admin during Render deploy

--- build.sh
+++ build.sh
@@ -6,4 +6,7 @@ python -m pip install -r requirements.txt
 
 python manage.py migrate --noinput
 python manage.py seed_plants
+if [ -n "${DJANGO_SUPERUSER_EMAIL:-}" ] && [ -n "${DJANGO_SUPERUSER_PASSWORD:-}" ]; then
+  python manage.py create_admin
+fi
 python manage.py collectstatic --noinput
