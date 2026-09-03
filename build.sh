832e3230a1c771eb986d066af864cbf75e908ace
triple98s <saidbinsuleiman5@gmail.com>
2026-09-03 20:20:07 +0300

    Fix Render dependency installation and port binding

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+
 python manage.py migrate --noinput
 python manage.py seed_plants
 python manage.py collectstatic --noinput
