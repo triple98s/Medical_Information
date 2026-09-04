77cf2ed8211c28bd6aed5c71461349c097017af0
triple98s <saidbinsuleiman5@gmail.com>
2026-09-04 09:08:43 +0300

    Configure Django default upload storage

--- plantmed/settings.py
+++ plantmed/settings.py
@@ -106,6 +106,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STORAGES = {
+    "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
     "staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"},
 }
 MEDIA_URL = "/media/"
